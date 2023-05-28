1900d470fc007e86c44d3daa170522bf2d3eff55
DebuggedMoon <predojevic@proton.me>
2023-05-28 02:02:50 +0200

    🚨💡 Resolve linter warnings for run.py

--- run.py
+++ run.py
@@ -1,8 +1,11 @@
+"""Main Programm for Initiating and Managing the Connect4 Game"""
+
 from terminalgame import TerminalGame
 from board import Board
 from game import Game
 
 def main():
+	"""Initiates a Connect4 Game for the Terminal"""
 
 	board = Board(6, 7)
 	game = Game(board)
