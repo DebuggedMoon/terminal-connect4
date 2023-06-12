385add0ec3eb80fefa820f066a880ea90ab4eeb7
DebuggedMoon <predojevic@proton.me>
2023-06-13 01:05:24 +0200

    🔨 Add methods for Input handling to TerminalGame

--- terminalgame.py
+++ terminalgame.py
@@ -17,7 +17,7 @@ class TerminalGame():
 		"""Draws content string at the given x and y position to the Terminal."""
 		with self.terminal.location(x_position, y_position):
 			print(content)
-   
+
 	def draw_gametitle(self):
 		"""Draws the game title to the Terminal."""
 
@@ -32,6 +32,45 @@ _/        _/    _/  _/    _/  _/    _/  _/        _/          _/          _/
 		for line in gametitle_lines:
 			print(self.terminal.center(self.terminal.color_rgb(24, 116, 205) + line + self.terminal.normal))
 
+	def draw_notification_message(self, message: str) -> None:
+		"""Draws a given text notification at the bottom of the Terminal."""
+		self.draw_at_xy(
+        	0,
+         	self.terminal.height - 2,
+          	self.terminal.white_on_red(self.terminal.center(message))
+        )
+
+	def get_player_move(self) -> int:
+		"""Handles user input and returns a valid player move."""
+		self.draw_at_xy(
+        	0,
+         	self.terminal.height - 3,
+          	self.terminal.white_on_blue(
+               self.terminal.center("> Press Column Number you wish to Select <")
+            )
+        )
+
+		while True:
+
+			with self.terminal.cbreak(), self.terminal.hidden_cursor():
+				player_input = self.terminal.inkey()
+
+			if not player_input.isdigit():
+				self.draw_notification_message(f"Input must be a number! Got: {player_input}")
+				continue
+
+			board_width = self.game.board.width
+			player_move = int(str(player_input))
+
+			if player_move <= 0 or player_move > board_width:
+				self.draw_notification_message(
+                    f"Input must be within board width (1-{board_width})! Got: {player_input}"
+                )
+			else:
+				return player_move
+
+		return player_move
+
 	def draw_board(self):
 		"""Draws the game board in its current state to the Terminal."""
 		for column in self.game.board.columns:
@@ -44,3 +83,4 @@ _/        _/    _/  _/    _/  _/    _/  _/        _/          _/          _/
 
 		self.draw_gametitle()
 		self.draw_board()
+		print(f"Player selected {self.get_player_move()}!")
