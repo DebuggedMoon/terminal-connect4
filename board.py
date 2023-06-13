d4aefca63d2f0936a53fdb54bbd1fe1b61b70471
DebuggedMoon <predojevic@proton.me>
2023-06-13 11:36:45 +0200

    🔨 Add get_surrounding_cells method to Board

--- board.py
+++ board.py
@@ -1,6 +1,6 @@
 """This module contains the game board class"""
 
-from typing import List
+from typing import List, Dict
 
 class Board():
 	"""
@@ -24,3 +24,16 @@ class Board():
 	def is_column_full(self, column_position: int) -> bool:
 		"""Returns true of the column is full / doesn't have any Empty cells left."""
 		return self.columns[column_position].count("Empty") == 0
+
+	def get_surrounding_cells(self, column_position: int, row_position: int) -> Dict[str, str]:
+		"""Returns a dict containing the cells surrounding the given position"""
+		return {
+			"TopLeft": self.columns[column_position - 1][row_position - 1],
+			"Top": self.columns[column_position][row_position - 1],
+			"TopRight": self.columns[column_position + 1][row_position - 1],
+			"Right": self.columns[column_position + 1][row_position],
+			"BottomRight": self.columns[column_position + 1][row_position + 1],
+			"Bottom": self.columns[column_position][row_position + 1],
+			"BottomLeft": self.columns[column_position - 1][row_position + 1],
+			"Left": self.columns[column_position - 1][row_position]
+		}
