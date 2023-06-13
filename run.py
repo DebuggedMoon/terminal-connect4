f6bce37acec83f06aaea52e833e587e4ecda3270
DebuggedMoon <predojevic@proton.me>
2023-06-13 13:49:04 +0200

    🔨 Add game logic for player winning

--- run.py
+++ run.py
@@ -17,8 +17,17 @@ def main():
 
 	while True:
 		player_move = terminal.get_player_move()
-		print(game.place_token("Player", player_move))
+		player_won = game.place_token("Player", player_move)
+
 		terminal.update_board()
+		if player_won:
+			terminal.draw_at_xy(
+       			0,
+          		int(terminal.height * 0.5),
+				terminal.white_on_green + terminal.center("!!! You WON !!!")
+            )
+			terminal.draw_notification_message("> PRESS `RUN PROGRAM`TO PLAY AGAIN <")
+			break
 
 if __name__ == "__main__":
 	main()
