aabc8d905b287228021936b4c93c87107d1b169c
DebuggedMoon <predojevic@proton.me>
2023-06-13 10:14:48 +0200

    🐛 Fix Board columns initiating incorrectly

--- board.py
+++ board.py
@@ -15,9 +15,8 @@ class Board():
 	def __init__(self, width: int, heigth: int):
 		self.width = width
 		self.heigth = heigth
-		self.columns = [["Empty"]*width]*heigth
+		self.columns = [["Empty"]*heigth]*width
 
 	def find_empty_column_index(self, column_position: int) -> int:
 		"""Returns the next empty column cells index."""
 		return self.columns[column_position].index("Empty")
-
